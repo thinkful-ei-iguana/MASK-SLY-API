3edd34f054ba043c5d5d6f04c1214535bd0eadac
AnthonyTB <anthonylol@protonmail.com>
2020-02-17 10:43:46 -0800

    merging with master

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://stephen@localhost/sly',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://stephen@localhost/sly-test',
+  DATABASE_URL:
+    process.env.DATABASE_URL || 'postgresql://postgres@localhost/sly',
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/sly-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'
 };
